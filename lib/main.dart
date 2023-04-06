0ef0bd0d6d3b50eb1ec3eeece68bbda332d4cd02
starlighter4097 <sonunigam4097@gmail.com>
2023-04-06 22:18:14 +0530

    added launcher icons:fix1

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'components/timertest.dart';
+// import 'components/timertest.dart';
 import 'timerapp.dart';
 
 void main() {
   runApp(MaterialApp(
-    routes: {
-      '/timertest': (context) => TimerTest(),
-    },
+    // routes: {
+    //   // '/timertest': (context) => TimerTest(),
+    // },
     theme: ThemeData.dark(
       useMaterial3: true,
     ),
