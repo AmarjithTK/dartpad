0c83e0c60bfccf77243cfb3b88f0651c85a745bd
starlighter4097 <sonunigam4097@gmail.com>
2023-04-06 15:31:02 +0530

    added assets/river-2.mp3

--- lib/timerapp.dart
+++ lib/timerapp.dart
@@ -1,6 +1,9 @@
+import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:flutter/material.dart';
 import 'package:sleek_circular_slider/sleek_circular_slider.dart';
 import 'dart:async';
+
+// add assets and improvise this app further in another day
 
 import 'package:timerapp/components/cardbutton.dart';
 
@@ -17,7 +20,9 @@ class _TimerAppState extends State<TimerApp> {
   // late int _initialDurationInSeconds = 0;
   // int _currentSliderValue = 5;
   bool _isRunning = false;
-  bool _isFinished = false;
+  bool _isFinished = true;
+
+  final AssetsAudioPlayer audioPlayer = AssetsAudioPlayer();
 
   List<int> presets = [
     1,
@@ -59,87 +64,88 @@ class _TimerAppState extends State<TimerApp> {
         title: Center(child: Text("Timer App")),
         actions: [Icon(Icons.menu_rounded)],
       ),
-      body: SafeArea(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        // mainAxisSize: MainAxisSize.max,
-        children: [
-          SleekCircularSlider(
-              innerWidget: (percentage) {
-                return Icon(
-                  Icons.timer,
-                  color: Colors.red,
-                  size: 50,
-                );
-              },
-              min: 5,
-              max: 121,
-              appearance: CircularSliderAppearance(
-                  size: 150,
-                  // size: 5,
-                  infoProperties: InfoProperties(
-                      mainLabelStyle:
-                          TextStyle(fontSize: 25, color: Colors.white))),
-              onChange: (double value) {
-                setState(() {
-                  if (_isRunning == false && _isFinished == true)
-                    _durationInSeconds = value.toInt() * 60;
-                });
-              }),
-          Container(
-            width: double.infinity,
-            height: 100,
-            child: Container(
-              decoration: BoxDecoration(
-                  // color: Colors.,
-                  border: Border.all(color: Color.fromARGB(255, 34, 34, 34)),
-                  borderRadius: BorderRadius.circular(4)),
-              child: Center(
-                  child: Text(formatDuration(_durationInSeconds),
-                      style: TextStyle(fontSize: 40.0))),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          // mainAxisSize: MainAxisSize.max,
+          children: [
+            SleekCircularSlider(
+                innerWidget: (percentage) {
+                  return Icon(
+                    Icons.work_history_rounded,
+                    color: Colors.white,
+                    size: 40,
+                  );
+                },
+                min: 5,
+                max: 121,
+                appearance: CircularSliderAppearance(
+                    size: 150,
+                    // size: 5,
+                    infoProperties: InfoProperties(
+                        mainLabelStyle:
+                            TextStyle(fontSize: 25, color: Colors.white))),
+                onChange: (double value) {
+                  setState(() {
+                    if (_isRunning == false && _isFinished == true)
+                      _durationInSeconds = value.toInt() * 60;
+                  });
+                }),
+            Container(
+              width: double.infinity,
+              height: 100,
+              child: Container(
+                decoration: BoxDecoration(
+                    // color: Colors.,
+                    border: Border.all(color: Color.fromARGB(255, 34, 34, 34)),
+                    borderRadius: BorderRadius.circular(4)),
+                child: Center(
+                    child: Text(formatDuration(_durationInSeconds),
+                        style: TextStyle(fontSize: 40.0))),
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        // _isRunning = !_isRunning; // true now
-                        if (_isRunning == false) {
-                          _durationInSeconds =
-                              _durationInSeconds > 0 ? _durationInSeconds : 0;
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          // _isRunning = !_isRunning; // true now
+                          if (_isRunning == false) {
+                            _durationInSeconds =
+                                _durationInSeconds > 0 ? _durationInSeconds : 0;
 
-                          startTimer();
-                          _isRunning = true;
-                        }
-                      });
-                    },
-                    child: Text('start')),
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        stopTimer();
-                      });
-                    },
-                    child: Text('stop')),
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        // _currentSliderValue = 5;
-                        _durationInSeconds = 0;
-                        resetTimer();
-                      });
-                    },
-                    child: Text('reset')),
-              ],
+                            startTimer();
+                            _isRunning = true;
+                          }
+                        });
+                      },
+                      child: Text('start')),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          stopTimer();
+                        });
+                      },
+                      child: Text('stop')),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          // _currentSliderValue = 5;
+                          _durationInSeconds = 0;
+                          resetTimer();
+                        });
+                      },
+                      child: Text('reset')),
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            child: GridView.builder(
+            GridView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 5,
                   mainAxisSpacing: 10.0,
@@ -174,10 +180,10 @@ class _TimerAppState extends State<TimerApp> {
                       ),
                     ),
                   );
-                }),
-          )
-        ],
-      )),
+                })
+          ],
+        ),
+      ),
     );
   }
 
@@ -186,7 +192,12 @@ class _TimerAppState extends State<TimerApp> {
     _timer = Timer.periodic(Duration(seconds: 1), (timer) {
       setState(() {
         _durationInSeconds--;
+        audioPlayer.open(Audio('assets/river-2.mp3'),
+            loopMode: LoopMode.single);
+        audioPlayer.play();
+
         if (_durationInSeconds <= 0) {
+          audioPlayer.stop();
           timer.cancel();
           _isRunning = false;
           _isFinished = true;
@@ -197,7 +208,7 @@ class _TimerAppState extends State<TimerApp> {
 
   void stopTimer() {
     _timer.cancel();
-
+    audioPlayer.stop();
     _isRunning = false;
   }
 
